fbef08504d3e2af438088635a4ba06c64e7bf2b1
Gijs <34489861+gijsaddas@users.noreply.github.com>
2020-05-20 15:00:12 +0200

    Data templating

--- quotes.py
+++ quotes.py
@@ -1,10 +1,20 @@
+import datetime
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.template_filter('datetime')
+def format_datetime(value):
+    date = datetime.datetime.fromtimestamp(value)
+    print(date.strftime('%Y-%m-%d %H:%M'))
+    return date.strftime('%Y-%m-%d %H:%M')
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    quotes = json.load(open('quotes.json'))
+    return render_template('index.html', quotes=quotes[:20])
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
